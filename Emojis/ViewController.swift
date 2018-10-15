b4a51319100cd7c6cbf6c4ebad27f2ddc7d63e74
Sue <tanakasuelen@gmail.com>
2018-10-14 22:32:56 -0700

    Initial Commit

---
+++ Emojis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emojis
+//
+//  Created by Suelen Tanaka on 2018-10-14.
+//  Copyright © 2018 Suelen Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
